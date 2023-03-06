e602fadb37629259afeb483b67c4fe3933762f85
anaskhan28 <khananasatique@gmail.com>
2023-03-06 16:34:59 +0530

    initial commit

---
+++ content-crafter/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App bg-black">
+      <h1 className="text-white">Hello</h1>
+     
+    </div>
+  )
+}
+
+export default App
